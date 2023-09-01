d60a81e97a0b435489c9ff536fea21cc75de07d8
ryangholland <rghcode@gmail.com>
2023-09-01 10:38:58 -0400

    Install React, Tailwind, DaisyUI

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <button className="btn">Hello Button</button>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/*eslint-env node*/
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
+
+
