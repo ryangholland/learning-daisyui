f2054d51a28cdc46d8f6a851a065f91294fbfdc7
ryangholland <rghcode@gmail.com>
2023-09-01 11:42:48 -0400

    Experiment with new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import "./App.css";
 import Navbar from "./assets/components/Navbar";
+import Hero from "./assets/components/Hero";
+import Footer from "./assets/components/Footer";
+import Card from "./assets/components/Card";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
+      <div className="flex justify-center gap-10 p-10 flex-wrap">
+        <Card />
+        <Card />
+        <Card />
+      </div>
+
+      <Footer />
     </>
   );
 }
